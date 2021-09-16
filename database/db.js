0798cb33eeb06dda715c7e5d3c3f01cf08132ff7
Brandao-lang <elijahbrandao@gmail.com>
2021-09-16 14:18:59 -0400

    server updates

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
 
-const connection = new Sequelize('postgres://ywzjfskzkofhdi:c82817d4ecd11e4b80d80d05cee8e3a2be06fe28da486a638fbfcf37a9652aa9@ec2-18-214-195-34.compute-1.amazonaws.com:5432/d846tgrbmnppg6', {
+const connection = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
